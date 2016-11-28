473844851a587ac7251ac5f0d259c44a1944e2bf
Jen Dixon <jendixon85@gmail.com>
2016-11-27 21:06:38 -0800

    Word break recursive challenge.

---
+++ word-break/word_break.py
@@ -0,0 +1,55 @@
+"""Parse an unbroken sentence into possible words.
+
+Example:
+
+    >>> sentences = parse('iatenoodlesfordinnertonight', vocab)
+
+    >>> for s in sorted(sentences):
+    ...    print s
+    i a ten oodles for dinner to night
+    i a ten oodles for dinner tonight
+    i a ten oodles ford inner to night
+    i a ten oodles ford inner tonight
+    i ate noodles for dinner to night
+    i ate noodles for dinner tonight
+    i ate noodles ford inner to night
+    i ate noodles ford inner tonight
+
+"""
+
+vocab = {'i', 'a', 'ten', 'oodles', 'ford', 'inner', 'to', 'night',
+         'ate', 'noodles', 'for', 'dinner', 'tonight'}
+
+
+def parse(phrase, vocab):
+    """Break a string into words.
+
+    Input:
+        - string of words without space breaks
+        - vocabulary (set of allowed words)
+
+    Output:
+        set of all possible ways to break this down, given a vocab
+    """
+
+    broken_phrases = set()
+    # base case: phrase matches word in vocab
+        # add to broken_phrases
+    for word in vocab:
+        if phrase == word:
+            broken_phrases.add(phrase)
+
+        elif phrase.startswith(word):
+            remainder = phrase[len(word):]
+
+            new_sentence = {word + ' ' + broken for broken in parse(remainder, vocab)}
+
+            broken_phrases.update(new_sentence)
+
+    return broken_phrases
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED; GREAT JOB! ***\n"
