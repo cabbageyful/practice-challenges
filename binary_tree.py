4630a9ab9f632e3aecc13edbfccae10526470a7e
Jen Dixon <jendixon85@gmail.com>
2017-01-12 16:48:14 -0800

    Binary tree implementation.

---
+++ binary_tree.py
@@ -0,0 +1,102 @@
+"""Binary Tree with in-order traversal
+
+    >>> zoo = BinaryTree()
+    >>> zoo.add_node('liger')
+    >>> zoo.add_node('puma')
+    >>> zoo.add_node('mountain lion')
+    >>> zoo.add_node('tiger')
+    >>> zoo.add_node('lion')
+    >>> zoo.add_node('cheetah')
+    >>> zoo.add_node('cougar')
+
+    >>> zoo.find_node('wolf')
+    False
+    >>> zoo.find_node('tiger')
+    True
+    >>> zoo.root
+    <Node: liger>
+    >>> zoo.root.left
+    <Node: cheetah>
+    >>> zoo.root.right
+    <Node: puma>
+    >>>
+"""
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return '<Node: {}>'.format(self.data)
+
+
+class BinaryTree(object):
+    def __init__(self, root=None):
+        self.root = root
+
+    def __repr__(self):
+        return '<Root: {}>'.format(self.root)
+
+
+    def add_node(self, new):
+        # if not issubclass(new, Node):
+        #     new = Node(new)
+
+        if self.root is None:
+            self.root = Node(new)
+
+        else:
+            self._add_node(new, self.root)
+
+    def _add_node(self, data, root):
+        if data < root.data:
+            if root.left is None:
+                root.left = Node(data)
+            else:
+                self._add_node(data, root.left)
+
+        elif data > root.data:
+            if root.right is None:
+                root.right = Node(data)
+            else:
+                self._add_node(data, root.right)
+
+        elif data == root.data:
+            return 'Item already exists in tree.'
+
+
+    def find_node(self, info):
+        if self.root != None:
+            self._find_node(info, self.root)
+        # if root is None or recursive call comes up empty, returns None
+        else:
+            return False
+
+
+    def _find_node(self, info, current_node):
+        if info == current_node.data:
+            return True
+        elif info < current_node.data and current_node.left != None:
+            self._find_node(info, current_node.left)
+        elif info > current_node.data and current_node.right != None:
+            self._find_node(info, current_node.right)
+
+    def print_bin_tree(self):
+        if self.root is not None:
+            return self._print_bin_tree(self.root)
+
+
+    def _print_bin_tree(self, current):
+        if current is not None:
+            self._print_bin_tree(current.left)
+            print current
+            self._print_bin_tree(current.right)
+
+
+#######################
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print '\nALL GOOD IN THE HOOD!!'
--- josephus.py
+++ josephus.py
@@ -1,18 +1,18 @@
-# """Given num_people in circle, kill [kill_every]th person, return survivor.
+"""Given num_people in circle, kill [kill_every]th person, return survivor.
 
-#     >>> find_survivor(4, 2)
-#     1
+    >>> find_survivor(4, 2)
+    1
 
-#     >>> find_survivor(41, 3)
-#     31
+    >>> find_survivor(41, 3)
+    31
 
 
-# As a sanity case, if never skip anyone, the last person will be our survivor:
+As a sanity case, if never skip anyone, the last person will be our survivor:
 
-#     >>> find_survivor(10, 1)
-#     10
+    >>> find_survivor(10, 1)
+    10
 
-# """
+"""
 
 
 class Node(object):
@@ -70,97 +70,13 @@ class Node(object):
 def find_survivor(num_people, kill_every):
     """Given num_people in circle, kill [kill_every]th person, return survivor."""
 
-    pass
+    target = Node.make_dll(num_people)
+
+    while target.next != target:
+        # counting from target, go to the killeveryth person
+        # 
 
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
         print "\n*** ALL TEST PASSED. W00T!\n"
-
-
-# class DoublyLinkedList(object):
-#     """docstring for DoublyLinkedList"""
-#     def __init__(self, first_node=None, last_node=None):
-#         """Initializes an empty head and tail on creation if none supplied."""
-
-#         assert isinstance(first_node, Node) or first_node is None
-#         assert isinstance(last_node, Node) or last_node is None
-#         self.first = first_node
-#         self.last = last_node
-
-#     def print_list(self):
-#         """Prints out the list."""
-#         current = self.first
-
-#         while current is not None:
-#             print current
-#             current = current.next
-
-#         return current
-
-#     def add_node_to_top(self, new_node):
-#         """Makes & adds node object to the beginning of doubly-linked list."""
-
-#         if not isinstance(new_node, Node):
-#             new_node = Node(new_node)
-
-#         # if no head, this node is head
-#         if self.first is None and self.last is None:
-#             self.first = new_node
-#             self.last = new_node
-
-#         # if there is a head, need to set current head to this node.next,
-#         # then change this node to head
-#         else:
-#             new_node.next = self.first
-#             self.first = new_node
-
-#     def add_node_to_end(self, new_node):
-#         """Adds a node to the end of the list."""
-
-#         if not isinstance(new_node, Node):
-#             new_node = Node(new_node)
-
-#         # if no tail & head, this node is the both
-#         if self.last is None and self.first is None:
-#             self.last = new_node
-#             self.first = new_node
-#         # else, set this node.prev to the current tail/last
-#         # then change the last node
-#         else:
-#             new_node.prev = self.last
-#             self.last = new_node
-
-#     def remove_node_by_index(self, index):
-#         """Given the index of a node, removes the node from the doubly-linked list."""
-#         # if head, then set head to head.next
-#         previous = None
-#         current = self.first
-#         counter = 0
-
-#         while current is not None and (counter < index):
-#             previous = current
-#             current = current.next
-#             counter += 1
-
-#         # at this point we're at our node, if index was first removed was the first/head
-#         if previous is None:
-#             self.first = current.next
-#         # if at another node in the list, then need to set the previous node's next to the current node's next
-#         else:
-#             previous.next = current.next
-
-#     def get_node_by_index(self, index):
-#         """Returns node data given an index."""
-
-#         if self.first is None:
-#             return None
-
-#         idx_counter = 0
-#         current = self.first
-
-#         while idx_counter < index:
-#             idx_counter += 1
-#             current = current.next
-
-#         return current    # should work if index isnt in range, should return None
